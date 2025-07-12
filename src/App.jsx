967f1060faa9e05cfafb54ef0d848148606a6713
Hinda216 <hindaho216@gmail.com>
2025-07-12 16:30:18 +1000

    done the first step Component Hierarchy by the react best practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Home from './components/Home'
+import Details from './components/Details'
+import Leaderboard from './components/Leaderboard'
+import Rewards from './components/Rewards'
+import Activity from './components/Activity'
+import Rules from './components/Rules'
+import AboutUS from './components/AboutUs'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return ()
+  return (
+    <>
+      <Header/>
+      <Home/>
+      <Details/>
+      <Leaderboard/>
+      <Activity/>
+      <Rewards/>
+      <Rules/>
+      <AboutUS/>
+      <Footer/>
+    </>
+  )
 }
 
 export default App
